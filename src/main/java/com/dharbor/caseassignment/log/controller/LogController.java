614d6f03283565e0fcd1ecbb3c4785868033b79c
Roushan Raja <roushanraja0@gmail.com>
2022-01-31 10:13:38 +0530

    good

--- src/main/java/com/dharbor/caseassignment/log/controller/LogController.java
+++ src/main/java/com/dharbor/caseassignment/log/controller/LogController.java
@@ -1,92 +1,128 @@
 package com.dharbor.caseassignment.log.controller;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.attribute.BasicFileAttributeView;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Dictionary;
-import java.util.Hashtable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
-import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-
-@RestController
+@Controller
 public class LogController {
+	String folderPath="D:/logs";
+	private static final String OUTPUT_ZIP_FILE = "D:/logs/log.zip";
 	
-    private static final String DIRECTORY = "D:/logs";
-    private static final String DATE = "27/01/2022";
-    
-    @Autowired
-    private ServletContext servletContext;
+	@RequestMapping("/logs")
+	@ResponseBody
+	public void show(@RequestParam("date") String date, HttpServletResponse response) throws IOException, ParseException {
+		
+		  List<String> fileNames = getFileName(date);
+		  CreateZip(fileNames);
+		  String fileName = OUTPUT_ZIP_FILE;
+		  response.setContentType("application/zip");
+	      response.setHeader("Content-Disposition", "attachment; filename=" +fileName);
+	      response.setHeader("Content-Transfer-Encoding", "binary");
+	      try {
+	    	  BufferedOutputStream bos = new BufferedOutputStream(response.getOutputStream());
+	    	  FileInputStream fis = new FileInputStream(fileName);
+	    	  int len;
+	    	  byte[] buf = new byte[1024];
+	    	  while((len = fis.read(buf)) > 0) {
+	    		  bos.write(buf,0,len);
+	    	  }
+	    	  fis.close();
+	    	  bos.close();
+	    	  response.flushBuffer();
+	      }
+	      catch(IOException e) {
+	    	  e.printStackTrace();
+	    	  
+	      }
+	}
 	
-    //Get the Logs
-    @GetMapping("/log")
-    public ResponseEntity<InputStreamResource> getLog(
-            @RequestParam(defaultValue = DATE) String date) throws IOException {
-
-    	String fileName = getFileName(date);
-//    	System.out.println(fileName);
-        MediaType mediaType = MediaTypeUtils.getMediaTypeForFileName(this.servletContext, fileName);
-//        System.out.println("fileName: " + fileName);
-//        System.out.println("mediaType: " + mediaType);
-//        getAttributes("D:/logs/log.txt");
-
-        File file = new File(DIRECTORY + "/" + fileName);
-        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
-
-        return ResponseEntity.ok()
-                // Content-Disposition
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + file.getName())
-                // Content-Type
-                .contentType(mediaType)
-                // Contet-Length
-                .contentLength(file.length()) //
-                .body(resource);
-    }
-    
-    //Get the properties of files to separate it by date
-    public String getAttributes(String pathStr) throws IOException {
-        Path p = Paths.get(pathStr);
-        BasicFileAttributes view
-           = Files.getFileAttributeView(p, BasicFileAttributeView.class)
-                  .readAttributes();
-        
-        long date = view.creationTime().toMillis();
-        ZonedDateTime t = Instant.ofEpochMilli(date).atZone(ZoneId.of("UTC"));
-        String dateCreated = DateTimeFormatter.ofPattern("dd/MM/yyyy").format(t);
-//        System.out.println(dateCreated);
-        return dateCreated;
-      }
-    
-    
-    public String getFileName(String date) throws IOException{
-    	String fileName = null;
-    	Dictionary<String, String> fileSystem = new Hashtable<String, String>();
-    	File[] files = new File(DIRECTORY).listFiles();
+    public List<String> getFileName(String date) throws IOException, ParseException{
+    	List<String> fileList= new ArrayList<String>();
+    	File[] files = new File(folderPath).listFiles();
     	 for (File filename : files) {
 //    		 System.out.println("File: "+ filename.getName());
     		 String fname = filename.getName();
-    		 String attr = getAttributes(DIRECTORY+"/"+fname);
-    		 fileSystem.put(attr, fname);
+    		 System.out.println(fname);
+    		 String time = GettingLastmodifiedTime(folderPath+"/"+fname);
+    		 SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+    		 int result = (formatter.parse(date)).compareTo(formatter.parse(time));
+    		 if(result == 0) {
+    			 fileList.add(fname);
+    		 }
     	 }
-    	 fileName = fileSystem.get(date);
-    	return fileName;
+    	 System.out.println(fileList);
+    	return fileList;
     }
+    
+    
+    public String GettingLastmodifiedTime(String path) {
+    	File file = new File(path);
+    	long lastModified = file.lastModified();
+    	Date date = new Date(lastModified);
+    	SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+    	String strDate = formatter.format(date);
+//    	System.out.println(strDate);
+    	return strDate;
+    }
+    
+    
+    public void CreateZip(List<String> fileName) throws IOException {
+
+        try {
+            
+            // create byte buffer
+            byte[] buffer = new byte[1024];
+ 
+            FileOutputStream fos = new FileOutputStream(OUTPUT_ZIP_FILE);
+ 
+            ZipOutputStream zos = new ZipOutputStream(fos);
+             
+            for (int i=0; i < fileName.size(); i++) {
+                 
+                File srcFile = new File(folderPath+"/"+fileName.get(i));
+ 
+                FileInputStream fis = new FileInputStream(srcFile);
+ 
+                // begin writing a new ZIP entry, positions the stream to the start of the entry data
+                zos.putNextEntry(new ZipEntry(srcFile.getName()));
+                 
+                int length;
+ 
+                while ((length = fis.read(buffer)) > 0) {
+                    zos.write(buffer, 0, length);
+                }
+ 
+                zos.closeEntry();
+ 
+                // close the InputStream
+                fis.close();
+                 
+            }
+ 
+            // close the ZipOutputStream
+            zos.close();
+             
+        }
+        catch (IOException ioe) {
+            System.out.println("Error creating zip file: " + ioe);
+        }
+    }
+
 }
